c719c82f9183095dbfad3176b719c36762bacd19
Carlos Yáñez <carlosyanez997@gmail.com>
2026-01-03 22:09:33 +0100

    Add solution for exercise 2 c5

--- 2c/ej2c5.py
+++ 2c/ej2c5.py
@@ -1,13 +1,13 @@
 """
 Enunciado:
-Desarrolla un conjunto de funciones en Python para manejar y procesar un conjunto de datos de calificaciones que 
+Desarrolla un conjunto de funciones en Python para manejar y procesar un conjunto de datos de calificaciones que
 potencialmente contiene valores faltantes o no válidos. El conjunto de datos está almacenado en un archivo CSV y
 contiene calificaciones en diversas materias para un grupo de estudiantes. El objetivo es limpiar los datos, eliminar
 filas con datos faltantes en columnas clave, y aplicar técnicas de imputación para tratar los valores faltantes en
 ciertas columnas.
 
 Las funciones a desarrollar son:
-1. Leer el archivo CSV: `read_csv(filepath: str)` que lee el archivo CSV desde la ruta especificada y devuelve un 
+1. Leer el archivo CSV: `read_csv(filepath: str)` que lee el archivo CSV desde la ruta especificada y devuelve un
 DataFrame de pandas.
 2. Limpiar el DataFrame: `clean_dataframe(df)` que limpia el DataFrame reemplazando valores no válidos ('Null', '-',
 'NA', 'na', y espacios en blanco) por `NaN`, y convierte las columnas numéricas a su tipo correspondiente, excepto la
@@ -15,17 +15,17 @@ primera columna.
 3. Eliminar filas con `NaN` en una columna específica: `dropna_specific_row_in_column(df, column_name: str)` que
 elimina las filas que contienen un valor `NaN` en la columna especificada por `column_name`.
 4. Rellenar valores faltantes en una columna específica: `fillna_method(df, column_name: str, fill_method='ffill',
-fill_value=None, limit=1)` que rellena los valores faltantes en la columna especificada utilizando un método de 
+fill_value=None, limit=1)` que rellena los valores faltantes en la columna especificada utilizando un método de
 relleno especificado (`ffill` para relleno hacia adelante con un límite de 1, o `mean` para rellenar con el promedio
 de la columna).
 
 Parámetros:
 - filepath (str): Ruta al archivo CSV que contiene los datos de calificaciones.
-- column_name (str): Nombre de la columna en la cual aplicar la eliminación de filas o el relleno de valores 
+- column_name (str): Nombre de la columna en la cual aplicar la eliminación de filas o el relleno de valores
 faltantes.
-- fill_method (str): Método de relleno para tratar valores faltantes (`ffill` para relleno hacia adelante, `mean` 
+- fill_method (str): Método de relleno para tratar valores faltantes (`ffill` para relleno hacia adelante, `mean`
 para el promedio).
-- fill_value (Optional[float]): Valor específico de relleno a usar si se especifica. Útil solo cuando 
+- fill_value (Optional[float]): Valor específico de relleno a usar si se especifica. Útil solo cuando
 `fill_method` es `mean`.
 - limit (int)`: Número máximo de rellenos consecutivos a aplicar cuando se utiliza `ffill`.
 
@@ -48,22 +48,35 @@ import numpy as np
 
 def read_csv(filepath):
     # Write here your code
-    pass
+    return pd.read_csv(filepath)
 
 
 def clean_dataframe(df):
     # Write here your code
-    pass
+    df = df.copy()
+
+    invalid_values = ["Null", "-", "NA", "na", " "]
+    df.replace(invalid_values, np.nan, inplace=True)
+    for col in df.columns[1:]:
+        df[col] = pd.to_numeric(df[col], errors="coerce")
+    return df
 
 
 def dropna_specific_row_in_column(df, column_name):
     # Write here your code
-    pass
+    return df.dropna(subset=[column_name])
 
 
 def fillna_method(df, column_name, fill_method="ffill", fill_value=None, limit=1):
     # Write here your code
-    pass
+    df = df.copy()
+
+    if fill_method == "ffill":
+        df[column_name] = df[column_name].fillna(method="ffill", limit=limit)
+    elif fill_method == "mean":
+        mean_value = fill_value if fill_value is not None else df[column_name].mean()
+        df[column_name] = df[column_name].fillna(mean_value)
+    return df
 
 
 # Para probar el código, descomenta las siguientes líneas y asegúrate de que el path al archivo sea correcto
